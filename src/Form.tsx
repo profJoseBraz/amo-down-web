7beb61966540879b8fa6b1d373d6dd2371b8a900
ZUMBI06 <bastoscamargojhonatas@gmail.com>
2024-07-12 11:48:54 -0300

    envio email

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,22 +1,59 @@
 import "./Form.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Form() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("tftv2024@tftv.io");
+    const [emailContado, setEmailContato] = useState("");
+    const [numero, setNumero] = useState("");
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        setEmail(email);
+    });
+
+    const handleSubmit = async (e : any) => {
+        e.preventDefault();
+        
+        const formData = {
+            name,
+            email,
+            message
+        };
+
+        try {
+            const response = await axios.post("https://amodown.tftv.io/envio-email.php", formData);
+            console.log(response.data);
+            // Adicione qualquer lógica adicional após o envio do formulário com sucesso
+        } catch (error) {
+            console.error("Erro ao enviar o formulário", error);
+            // Adicione lógica para lidar com o erro
+        };
+    };
+
+    useEffect(() => {
+        setMessage(`${message}
+            ${emailContado}
+            ${numero}`);
+    }, [message, emailContado, numero]);
+
     return (
         <>
             <div className="form">
                 <h1>Envie uma Mensagem</h1>
                 <div className="line"></div>
                 <h3>Um email será enviado para o proprietário</h3>
-                <form action="">
+                <form>
                     <div className="all-inputs">
-                        <textarea placeholder="Escreva sua mensagem"></textarea>
+                        <textarea placeholder="Escreva sua mensagem" onChange={(e) => {setMessage(e.target.value)}}></textarea>
                         <div className="inputs">
-                            <input type="text" placeholder="Seu nome"/>
-                            <input type="text" placeholder="Seu email"/>
-                            <input type="text" placeholder="Seu número de telefone"/>
+                            <input type="text" placeholder="Seu nome" onChange={(e) => {setName(e.target.value)}}/>
+                            <input type="text" placeholder="Seu email" onChange={(e) => {setEmailContato(e.target.value)}}/>
+                            <input type="text" placeholder="Seu número de telefone" onChange={(e) => {setNumero(e.target.value)}}/>
                         </div>
                     </div>
-                    <button type="submit">Envie a Mensagem</button>
+                    <button onClick={handleSubmit}>Envie a Mensagem</button>
                 </form>
             </div>
         </>
