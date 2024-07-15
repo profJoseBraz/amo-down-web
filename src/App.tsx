fe2ab10d66f846162dcf2312ef5779fd0d6c0f12
ZUMBI06 <bastoscamargojhonatas@gmail.com>
2024-07-15 10:36:29 -0300

    media query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import HorarioDeFuncionamento from './assets/HorarioDeFuncionamento'
+import Footer from './Footer'
+import Form from './Form'
+import Header from './Header'
+import Sobre from './Sobre'
 
 function App() {
   return (
     <>
-      
+      <Header></Header>
+      <Sobre></Sobre>
+      <HorarioDeFuncionamento></HorarioDeFuncionamento>
+      <Form></Form>
+      <Footer></Footer>
     </>
   )
 }
