81edce1b35b832ac72502166bdf6f46edb33b0be
ZUMBI06 <bastoscamargojhonatas@gmail.com>
2024-07-15 09:31:17 -0300

    estilizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import './App.css'
 
-import Footer from './Footer'
-import Form from './Form'
-import Location from './Location'
-
-
 function App() {
   return (
     <>
-      <Location></Location>
-      <Form></Form>
-      <Footer></Footer>
+      
     </>
   )
 }
