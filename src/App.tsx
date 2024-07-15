fd14d0d51474d6bb9ff40fd1c0009d903205299e
profjosebraz <prof.josebrazz@gmail.com>
2024-07-15 09:28:14 -0300

    app tsx clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import './App.css'
 
-import Footer from './Footer'
-import Header from './Header'
-import HorarioDeFuncionamento from './assets/HorarioDeFuncionamento'
-import Horario from './assets/HorarioDeFuncionamento'
-
 function App() {
   return (
     <>
-      {/* <Header></Header> */}
-      <HorarioDeFuncionamento></HorarioDeFuncionamento>
+      
     </>
   )
 }
