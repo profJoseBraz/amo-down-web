19e4325d9f7c6a0c31fea053e7b8d58abac0305a
RICARDO BATISTA <batista.4995@aluno.pr.senac.br>
2024-07-22 09:00:13 -0300

    sobre2207

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import HorarioDeFuncionamento from './assets/HorarioDeFuncionamento'
 import Footer from './Footer'
 import Form from './Form'
 import Header from './Header'
+import Location from './Location'
 import Sobre from './Sobre'
 
 function App() {
   return (
     <>
-
+      <Header/>
+      <Sobre/>
+      <HorarioDeFuncionamento/>
+      <Form/>
+      <Location/>
+      <Footer/>
     </>
   )
 }
